7b39abeee977020ed121966bac723f606b621ae3
Afonso Dolmen <afonsodolmensilva@outlook.com>
2025-02-10 16:07:48 -0300

    Configurado conexão com postgresql e realizado migrações

---
+++ app/db/connection.py
@@ -0,0 +1,10 @@
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DB_URL = getenv('DB_URL')
+
+# Criando nossa engine e sessão do banco
+engine = create_engine(DB_URL, pool_pre_ping=True)
+Session = sessionmaker()
---
+++ app/db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+
+
+class Category(Base):
+    """
+    Cria a tabela de categorias no banco de dados
+    """
+    __tablename__ = 'categories'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String, nullable=False)
+    slug = Column('slug', String, nullable=False)
