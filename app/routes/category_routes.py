482a371d3abcb7ae4c6d6b896c54cdbbc049695e
Afonso Dolmen <afonsodolmensilva@outlook.com>
2025-02-12 08:22:21 -0300

    Implementado teste e rota para delete

--- app/routes/category_routes.py
+++ app/routes/category_routes.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, status
-from fastapi import Depends
+from fastapi import (
+    APIRouter,
+    Depends,
+    Response,
+    status
+)
 from sqlalchemy.orm import Session
 from app.schemas.category import Category
 from app.routes.deps import get_db_session
@@ -29,3 +33,14 @@ def list_categories(
     uc = CategoryUseCases(db_session)
 
     return uc.list_categories()
+
+
+@router.delete('/delete/{id}')
+def delete_category(
+    id: int,
+    db_session: Session = Depends(get_db_session)
+):
+    uc = CategoryUseCases(db_session=db_session)
+    uc.delete_category(id)
+
+    return Response(status_code=status.HTTP_200_OK)
