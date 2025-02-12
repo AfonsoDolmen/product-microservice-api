2af3fd934a95b91d4217514a2f94750b712eda18
Afonso Dolmen <afonsodolmensilva@outlook.com>
2025-02-12 08:04:50 -0300

    Implementado teste e feature para deletar um category

--- app/test/use_cases/test_category_use_cases.py
+++ app/test/use_cases/test_category_use_cases.py
@@ -1,9 +1,10 @@
+import pytest
 from sqlalchemy.orm import Session
 from typing import List
+from fastapi import HTTPException
 from app.use_cases.category import CategoryUseCases
 from app.db.models import Category as CategoryModel
 from app.schemas.category import Category, CategoryOutput
-from app.test.conftest import db_session
 
 
 def test_add_category_uc(db_session: Session):
@@ -42,3 +43,30 @@ def test_list_categories(db_session: Session, categories_on_db: List[Category]):
     assert categories[0].id == categories_on_db[0].id
     assert categories[0].name == categories_on_db[0].name
     assert categories[0].slug == categories_on_db[0].slug
+
+
+def test_delete_category(db_session: Session):
+    """
+    Testando ao deletar um registro
+    """
+    category_model = CategoryModel(name='Roupa', slug='roupa')
+    db_session.add(category_model)
+    db_session.commit()
+
+    uc = CategoryUseCases(db_session=db_session)
+    uc.delete_category(id=category_model.id)
+
+    category_model = db_session.query(
+        CategoryModel).filter_by(id=category_model.id).first()
+
+    assert category_model is None
+
+
+def test_delete_category_non_exist(db_session: Session):
+    """
+    Testando ao deletar um registro que não existe
+    """
+    uc = CategoryUseCases(db_session=db_session)
+
+    with pytest.raises(HTTPException):
+        uc.delete_category(id=1)
