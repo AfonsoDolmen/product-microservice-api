12636ed61f2d7fe52b13f79fbbbc85b893a14779
Afonso Dolmen <afonsodolmensilva@outlook.com>
2025-02-14 11:57:08 -0300

    Implementado rota put e testes

--- app/test/routes/test_product_routes.py
+++ app/test/routes/test_product_routes.py
@@ -1,6 +1,7 @@
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
-from fastapi import status
+from fastapi import HTTPException, status
 from typing import List
 from app.db.models import Product as ProductModel
 from app.db.models import Category as CategoryModel
@@ -56,3 +57,36 @@ def test_add_product_route_invalid_category_slug(db_session: Session):
     products_on_db = db_session.query(ProductModel).all()
 
     assert len(products_on_db) == 0
+
+
+def test_update_product_route(db_session: Session, product_on_db):
+    body = {
+        'name': 'Updated Camisa',
+        'slug': 'updated-camisa',
+        'price': 100.99,
+        'stock': 20
+    }
+
+    response = client.put(f'/product/update/{product_on_db.id}', json=body)
+
+    assert response.status_code == status.HTTP_200_OK
+
+    db_session.refresh(product_on_db)
+
+    assert product_on_db.name == 'Updated Camisa'
+    assert product_on_db.slug == 'updated-camisa'
+    assert product_on_db.price == 100.99
+    assert product_on_db.stock == 20
+
+
+def test_update_product_route_invalid_id():
+    body = {
+        'name': 'Updated Camisa',
+        'slug': 'updated-camisa',
+        'price': 100.99,
+        'stock': 20
+    }
+
+    response = client.put('/product/update/1', json=body)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
